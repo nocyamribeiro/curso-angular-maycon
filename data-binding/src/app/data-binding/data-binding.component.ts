32f582d80d59e86685c17ca7a381b51cf6690ce9
Maycon <nocyamribeiro@gmail.com>
2020-10-20 19:25:12 -0300

    Aulas de DataBinding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url = 'http://loiane.com';
+  cursoAngular = true;
+  urlImagem = 'https://s2.glbimg.com/nksCDlmBan7iiSCgJqZdN7A5ekc=/e.glbimg.com/og/ed/f/original/2019/10/22/6th-place_small-white-hair-spider_javier-ruperez_nikon-small-world.jpg';
+
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
+  nomeDoCurso: string = 'Angular';
+
+  valorInicial = 15;
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento){
+    console.log(evento.novoValor);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ primeiro-projeto/src/app/cursos/cursos.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  constructor() { }
+
+  // tslint:disable-next-line: typedef
+  getCursos() {
+    return ['Java', 'Ext JS', 'Angular', 'Javascript'];
+  }
+}

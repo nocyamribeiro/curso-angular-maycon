750e4c4db5af8dcaa7adf674992970180be47d01
Maycon <nocyamribeiro@gmail.com>
2020-11-04 16:24:29 -0300

    Criação do projeto para aula de formulários em Angular

---
+++ forms/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+
+const routes: Routes = [
+  { path: 'dataForm', component: DataFormComponent },
+  { path: 'templateForm', component: TemplateFormComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { DataFormComponent } from './data-form/data-form.component'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
